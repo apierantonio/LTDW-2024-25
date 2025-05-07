86fa459079adaa6c7dfd5c18acc6d02c5aff8851
Alfonso Pierantonio <alfonso.pierantonio@univaq.it>
2025-05-07 12:42:53 +0200

    php 1

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<html>
+    
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <h1>PHP Test</h1>
+    <p>This is a test page for PHP.</p>
+    <?php
+        echo "<p>Hello, World!</p>";
+    ?>
+</html>
